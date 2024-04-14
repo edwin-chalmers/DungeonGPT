10f36ec2f7907ba8883ca72ea2993e835073e9a9
Edwin Chalmers <edwinchalmers@gmail.com>
2024-04-14 13:59:07 -0600

    Add layout with styling

---
+++ src/Components/App/App.styled.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+
+
+
+const centerElements = () => `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const StyledApp = styled.div`
+    --main-color: #8BE8D7;
+    --secondary-color: #39615A;
+    --background-color: #283134;
+
+
+    width: 100%;
+    min-height: 100vh;
+    background-color: var(--background-color);
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    color: #8BE8D7;
+`
+
+export const Nav = styled.nav`
+    width: 800px;
+    height: 50px;
+    font-weight: 800;
+    ${centerElements}
+`
+
+export const PlayerInfo = styled.div`
+    top: 50%;
+    left: 10%;
+
+    width: 250px;
+    height: 250px;
+    border: 1px solid red;
+    box-sizing: border-box;
+
+    text-align: right;
+    position: absolute;
+`
+
---
+++ src/Components/ChatBox/ChatBox.styled.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+
+
+export const StyledChatBox = styled.div`
+    width: 800px;
+    min-height: 50px;
+    max-height: 200px;
+    border: 1px solid var(--secondary-color);
+    box-sizing: border-box;
+    background-color: rgba(0,0,0,0);
+    margin-bottom: 20px;
+    border-radius: 15px;
+    padding-left: 10px;
+    
+    display: flex;
+    align-items: center;
+    justify-content: right;
+    
+    textarea {
+        box-sizing: border-box;
+        background-color: rgba(0,0,0,0);
+        width: 100%;
+        border: none;
+        color: var(--main-color);
+        resize: none;
+        outline: none;
+        padding: 12px 0 0 0;
+    }
+
+    &:focus-within {
+    border-color: var(--main-color);
+    }
+`
+
+export const ChatMessage = styled.div`
+
+`
+
+export const EnterButton = styled.button`
+    --main-color: #8BE8D7;
+    --secondary-color: #39615A;
+    --background-color: #283134;
+
+    width: 35px;
+    height: 35px;
+    background-color: rgba(0,0,0,0);
+    border: 1px solid var(--secondary-color);
+    border-radius: 12px;
+    margin-right: 10px;
+    color: var(--background-color);
+    background-color: var(--secondary-color);
+    font-weight: 800;
+
+    background-color: ${props => props.disabled ? 'var(--secondary-color)' : 'var(--main-color)'};
+    cursor: ${props => props.disabled ? '' : 'pointer'}
+`
